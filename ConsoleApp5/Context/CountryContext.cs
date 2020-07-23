7e9eb43560fa806a39579a06ba227527109d403a
Igor Poluboyarov <ipoluboyarov@gmail.com>
2020-07-23 18:54:53 +0300

    Add project files.

---
+++ ConsoleApp5/Context/CountryContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using ConsoleApp5.Entity;
+
+namespace ConsoleApp5
+{
+    class CountryContext : DbContext
+    {
+        public CountryContext() : base("DbConnection")
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Coin> Coins { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>()
+                .HasMany(c => c.Neighbors)
+                .WithMany()
+                .Map(m =>
+                {
+                    m.MapLeftKey("CountryId");
+                    m.MapRightKey("NeighborId");
+                    m.ToTable("CountryNeighbor");
+                });
+        }
+
+
+    }
+}
---
+++ ConsoleApp5/Entity/Country.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleApp5.Entity
+{
+    public sealed class Country
+    {
+        public Country()
+        {
+            Neighbors = new List<Country>();
+        }
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Country> Neighbors { get; set; }
+    }
+
+    public sealed class Coin
+    {
+        public Coin()
+        {
+            Transactions = new List<Transaction>();
+        }
+
+        //[Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        [ForeignKey("Country")]
+        public int Id { get; set; }
+        public Country Country { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public virtual Country Sender { get; set; }
+        public virtual Country Recipient { get; set; }
+        public virtual Coin Coin { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class Balance
+    {
+        public virtual Coin Coin { get; set; }
+        public int Count { get; set; }
+    }
+}
